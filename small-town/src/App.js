1a5541bcee1f2c30855d3301c99c3518e8b9f6ab
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2024-11-12 15:21:05 +0000

    Project structure and Village Class

---
+++ small-town/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VillageInfo from './Village'
+
+function App() {
+  return (
+    <main>
+      <VillageInfo villageName={"NameTest"}/>
+    </main>
+  );
+}
+
+export default App;

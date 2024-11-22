ee7c7d414df6d9032b9db75e07b93cfef825be7b
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2024-11-22 18:17:36 +0000

    Refactored village to Village

--- small-town/src/Villager.js
+++ small-town/src/Villager.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Villager.css';
 import VillagerCard from './VillagerCard';
 
+/*
+  - Create message when new villager is added
+  - Add button to show all villagers
+*/
+
 const Villager = ({ createVillager, villagers }) => {
   return (
     <>

2a1cf451408cdfa5fa5965a2e8c104e3806438f3
SergiooOliveira <sergioooliveira908@gmail.com>
2024-11-25 15:53:55 +0000

    Villager Card visual update

--- small-town/src/VillagerCard.js
+++ small-town/src/VillagerCard.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './VillagerCard.css';
 
+/*#Region To-do
+  - fix Image not showing
+*/
+
 const VillagerCard = ({ villagers }) => {
+
+  const handleClick = () => {
+    console.log("Ver mais")
+  }
+
   return (
     <>
     <div className='villagerList'>
-        {villagers.map(villager => (                 
-            <div className='card' key={villager.id}>
-                <div className='face'>
-                    
-                </div>
-                <div className='name'>
-                    <h4>{villager.name}</h4>
+        {villagers.map(villager => (
+          <div key={villager.id}>
+            <div className='card'>
+                <img src="images/200x150.png" alt="Character"/>
+                <div className='card-details'>
+                    <span>{villager.name}</span>
+                    <span className="more-options" onClick={handleClick}>...</span>
                 </div>
             </div>
+            <div className="card" key={villager.id}>    
+              <div className="card-expanded">
+                <p><strong>Job:</strong> </p>
+                <p><strong>Age:</strong> </p>
+                <p><strong>Stats:</strong> </p>
+                <p><strong>Inventory:</strong> </p>
+              </div>
+              <div className="card-details">
+                <span>{villager.name}</span>
+                <span className="more-options">...</span>
+              </div>
+            </div>
+          </div>
         ))}
     </div> 
     </>

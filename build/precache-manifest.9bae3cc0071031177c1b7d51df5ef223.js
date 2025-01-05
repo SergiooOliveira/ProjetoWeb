e253503beaaa0695239f10d5c4ef14c48f5daedd
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2025-01-05 19:18:31 +0000

    update

---
+++ build/precache-manifest.9bae3cc0071031177c1b7d51df5ef223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657f5f370417cf9add34edb8b0b0e9b2",
+    "url": "/ProjetoWeb/index.html"
+  },
+  {
+    "revision": "2b7f4c6d62808d99db5c",
+    "url": "/ProjetoWeb/static/css/main.512c87cd.chunk.css"
+  },
+  {
+    "revision": "04da5dba4cd640d3ffc4",
+    "url": "/ProjetoWeb/static/js/2.2d477f3b.chunk.js"
+  },
+  {
+    "revision": "2b7f4c6d62808d99db5c",
+    "url": "/ProjetoWeb/static/js/main.f8516fcc.chunk.js"
+  },
+  {
+    "revision": "c1305f5c7d146916fab0",
+    "url": "/ProjetoWeb/static/js/runtime~main.f794a3fd.js"
+  }
+]);
